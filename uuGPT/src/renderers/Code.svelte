ee943aeed7d6176bbda1aedb04fc1fc4352d3334
hecatechin1 <wallace0079@foxmail.com>
2024-09-21 20:15:05 +0800

    上传项目源码

---
+++ uuGPT/src/renderers/Code.svelte
@@ -0,0 +1,128 @@
+<script>
+  import { onMount, afterUpdate } from 'svelte';
+  import { t } from 'svelte-i18n';
+  import CopyIcon from "../assets/copy.svg";
+  import CheckIcon from "../assets/check.svg";
+  import hljs from "highlight.js";
+  import { marked } from "marked";
+  import "highlight.js/styles/default.css";
+  import { isStreaming } from "../stores/stores";
+  import { get } from 'svelte/store';
+    import Html from './Html.svelte';
+
+  export let text=""; // Markdown 中的代码内容
+  export let language="";  // 代码语言
+  let highlightedCode = "";
+  let hj= hljs.highlightAuto(text);
+
+  // if(!get(isStreaming)){
+  //   hj = hljs.highlightAuto(text);
+  // }
+  // let  hj = hljs.highlightAuto(text.replace('\\n', '\n'));
+
+  let copied = false;
+
+// 高亮代码函数
+const highlightCode = () => {
+    if (language) {
+      highlightedCode = hljs.highlight(language, text).value;
+    } else {
+      highlightedCode = hljs.highlightAuto(text).value;
+    }
+  };
+
+   // 初次渲染时高亮代码
+   onMount(() => {
+    highlightCode();
+  });
+
+  // 每次 text 更新时重新高亮
+  afterUpdate(() => {
+    highlightCode();
+  });
+  
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      copied = true;  // 设置为 true 显示成功消息
+      setTimeout(() => {
+        copied = false;  // 3 秒后隐藏成功消息
+      }, 2000);
+    } catch (error) {
+      console.error("Error copying text: ", error);
+    }
+  };
+</script>
+
+<div style="position:relative">
+  <div class="copycode">
+    <div>{language}</div>
+    <button class="" on:click={copyToClipboard}>
+      {#if copied}
+      <img class="inline-block" alt="Copied" src={CheckIcon} />
+      
+        <span>{$t('code.copied')}!</span>
+      {:else}
+      <img class="inline-block copycode-icon" alt="Copy Code" src={CopyIcon} />
+      <span>{$t('code.copy')}</span>
+      {/if}
+    </button>
+  </div>
+  <pre><code id='testid'>{@html highlightedCode}</code></pre>
+</div>
+
+<style>
+  pre {
+    background-color: #0d0d0d;
+    border-radius: 0px 0px 10px 10px;
+    padding: 20px;
+    margin: 0 20px 0 20px;
+    opacity: 0;
+    animation: fade-in 0.5s ease-in-out forwards;
+    margin-bottom: 1rem;
+    font-weight: bold;
+    overflow-wrap: break-word;
+    white-space: pre-wrap;
+  }
+
+  .copycode {
+    display: flex;
+    justify-content: space-between;
+    background-color: #333;
+    margin: 0 20px 0 20px;
+    border-radius: 10px 10px 0px 0px;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    color: rgb(187, 187, 187);
+  }
+
+  button {
+    font-size: small;
+    display: block;
+    transition: all 0.1s ease-in-out;
+    color: rgb(187, 187, 187);
+  }
+
+  button:hover {
+    color: white;
+  }
+
+  .copycode button img {
+    margin: 0;
+    filter: brightness(0) invert(0.7);
+  }
+
+  .copycode button:hover img {
+    filter: brightness(0) invert(1);
+  }
+
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+
+</style>
---
+++ uuGPT/src/services/uuAIServices.ts
@@ -0,0 +1,185 @@
+import { SSE } from 'sse.js';
+import { get, writable } from 'svelte/store';
+import {messages, userRequestedStreamClosure,isStreaming ,streamContext} from "../stores/stores";
+import {setMessagesHistory} from "../manages/messageManages";
+import { countTicks } from '../utils/generalUtils';
+import { t } from "svelte-i18n";
+
+let currentmid = null;
+let globalSource: EventSource | null = null;  
+
+let timeoutId: NodeJS.Timeout | null = null; // 用于超时检测
+export const closeStream = async()=>{
+    console.log(currentmid);
+    globalSource?.close();
+    let cm = get(messages);
+    if(currentmid<0){
+        currentmid = cm.length-1;
+    }
+    cm[currentmid].content = cm[currentmid].content.replace(/█+$/, '') ||  get(t)("app.linkError");
+    setMessagesHistory(cm);
+    isStreaming.set(false);
+    currentmid = null;
+}
+
+
+const resetTimeout = (source: EventSource, currentMessages: any[], mid: number, streamText: string) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+  
+    timeoutId = setTimeout(() => {
+      // 10秒内未收到任何消息，认为连接失败
+    //   console.error('10秒内未收到任何消息，连接失败');
+      source.close();
+      isStreaming.set(false);
+        
+      if (mid < 0) {
+        setMessagesHistory([...currentMessages, {
+          role: 'assistant',
+          content: streamText.replace(/█+$/, '') || get(t)("app.linkError"),
+          isLiked: false,
+          isDisliked: false
+        }]);
+      } else {
+        currentMessages[mid].content = streamText.replace(/█+$/, '') ||  get(t)("app.linkError");
+        setMessagesHistory(currentMessages);
+      }
+  
+      streamText = ''; // 清空streamText
+      currentmid = null;
+    }, 20*1000); // 20 秒超时
+  };
+
+export async function sendMessage(msg:any,mid:number) {
+
+    userRequestedStreamClosure.set(false);
+    let hasError = false;
+    let currentMessages = get(messages);
+    let tickCounter =0;
+    let ticks = false;
+    currentMessages = [...currentMessages];
+    if(mid <0){
+        setMessagesHistory([...currentMessages,{
+            role:'assistant',
+            content:"█",
+            isLiked:false,
+            isDisliked:false
+        }]);
+    }else{
+        currentMessages[mid].content = "█";
+
+            // currentMessages[mid].content = streamText;
+        
+        setMessagesHistory(currentMessages);
+    }
+
+    let done = false;
+    let streamText = "";
+    isStreaming.set(true);
+    let source = new SSE("https://api.uugpt.com/ai/stream",{
+        headers:{
+            "Content-Type": "application/json",
+        },
+        method:"POST",
+        payload: JSON.stringify({
+            system:"All responses must be in Markdown format.Don't use '---' as devide line.",
+            prompt:msg
+        })
+    });
+
+    source.addEventListener("message",async(e:any)=>{
+        resetTimeout(source, currentMessages, mid, streamText);
+        if(e.data !== "[DONE]"){
+            try{
+                if(!hasError){
+                    let msgTicks = countTicks(e.data);
+                    tickCounter += msgTicks;
+                    if (msgTicks === 0) tickCounter = 0;
+                    if (tickCounter === 3) {
+                      ticks = !ticks;
+                      tickCounter = 0;
+                    }
+                    streamText += e.data;
+                    streamContext.set({ streamText});
+
+                    if(mid <0){
+                        setMessagesHistory([...currentMessages,{
+                            role:'assistant',
+                            // content:streamText +  "█"+ (ticks ? "\n```" : ""),
+                            content:streamText +  "█",
+                            isLiked:false,
+                            isDisliked:false
+                        }]);
+                    }else{
+                        currentMessages[mid].content = streamText+"█";
+
+                            // currentMessages[mid].content = streamText;
+                        
+                        setMessagesHistory(currentMessages);
+                    }
+
+                }
+            }catch(error){
+                hasError = true;
+                source.close();
+                isStreaming.set(false);
+                return;
+            }
+        }else{
+            streamText = streamText.replace(/█+$/, '');
+            // console.log(streamText);
+            done = true;
+            isStreaming.set(false);
+            source.close();
+
+            if(mid <0){
+                setMessagesHistory([...currentMessages,{
+                    role:'assistant',
+                    content:streamText,
+                    isLiked:false,
+                    isDisliked:false
+                }]);
+            }else{
+
+                    currentMessages[mid].content = streamText;
+
+                setMessagesHistory(currentMessages);
+            }
+
+
+            streamText = "";
+            currentmid = null;
+        }
+
+    });
+
+    source.addEventListener('error',()=>{
+        resetTimeout(source, currentMessages, mid, streamText);
+        streamText = streamText.replace(/█+$/, '');
+        isStreaming.set(false);
+        source.close();
+        if(mid <0){
+            setMessagesHistory([...currentMessages,{
+                role:'assistant',
+                content:streamText,
+                isLiked:false,
+                isDisliked:false
+            }]);
+        }else{
+
+                currentMessages[mid].content = streamText;
+            
+            setMessagesHistory(currentMessages);
+        }
+
+        streamText = "";
+        currentmid=null;
+    });
+
+    source.stream();  
+    globalSource = source;  
+    currentmid = mid;
+     // 开始时初始化超时检测
+    resetTimeout(source, currentMessages, mid, streamText);
+}
